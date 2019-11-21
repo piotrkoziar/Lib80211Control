279681a58e8b7fa6e7b8ee6fe1a3c215c08c8e30
piotrkoziar <piotrkoziar@hotmail.com>
2019-11-22 00:40:20 +0100

    Added print functions

--- src/Wiphy.h
+++ src/Wiphy.h
@@ -7,14 +7,20 @@
 namespace wiphynlcontrol {
 
 class Wiphy : public Entity {
+  // For better appearance.
+  using sp_uint32 = std::shared_ptr<uint32_t>;
+  using sp_string = std::shared_ptr<std::string>;
+
  public:
   explicit Wiphy(const uint32_t &id);
 
  public:
-  std::shared_ptr<uint32_t>    id_;
-  std::shared_ptr<std::string> name_;
-  std::shared_ptr<std::string> address_;
-  std::shared_ptr<uint32_t>    frequency_;
+  sp_uint32 id_;
+  sp_string name_;
+  sp_uint32 bands_;
+  sp_uint32 txq_params_;
+  sp_uint32 frequency_;
+  sp_uint32 channel_type_;
 
  public:
   // Returns identifier. Overloads abstract method from Entity.
